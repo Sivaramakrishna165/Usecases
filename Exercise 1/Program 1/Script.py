64bcfe795fee2a73505e6a712c83fc9cfde35a79
sai <sai@gnail.com>
2024-11-04 17:27:47 +0530

    'updated'

---
+++ Exercise 1/Program 1/Script.py
@@ -0,0 +1,14 @@
+import json
+
+# Sample input JSON array
+input_data = [
+    {"name": "Person1", "height": 158},
+    {"name": "Person2", "height": 174},
+    {"name": "Person3", "height": 146}
+]
+
+# Sorting the JSON array by the height attribute
+sorted_data = sorted(input_data, key=lambda x: x["height"])
+
+# Printing the sorted result
+print(json.dumps(sorted_data, indent=4))
